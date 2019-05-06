4e147ca6d689d8f9653f14575024e8468a935a71
Lee McPherson <leemcpherson@hotmail.com>
2019-05-05 17:10:58 -0700

    interesting fixes to make responsive menu work right

--- BlazorUIComponents.Demo/wwwroot/semanticTransitions.js
+++ BlazorUIComponents.Demo/wwwroot/semanticTransitions.js
@@ -1,18 +1,30 @@
 ﻿function showMenu(element) {
-    $(element).removeClass("collapse");
-    $(element).transition({
-        animation: 'slide down in',
-        onComplete: function () {
+    //var x = window.matchMedia("(max-width: 767.98px)");
+    //if (x.matches) {
+        $(element).removeClass("collapse");
+        $(element).transition({
+            animation: 'slide down in',
+            onComplete: function () {
 
-        }
-    });
+            }
+        });
+    //}
 }
 
 function hideMenu(element) {
-    $(element).transition({
-        animation: 'slide down out',
-        onComplete: function () {
-            $(element).addClass("collapse");
-        }
-    });
+    //var x = window.matchMedia("(max-width: 767.98px)");
+    //if (x.matches) {
+        $(element).transition({
+            animation: 'slide down out',
+            onComplete: function () {
+                $(element).addClass("collapse");
+                $(element).removeClass("hidden");
+            }
+        });
+    //}
+}
+
+function matchesMediaQuery(query) {
+    var x = window.matchMedia(query);
+    return x.matches;
 }
